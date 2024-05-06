61c9aaa1fb944b40459f1349ff5536571e5d2e20
Matthieu <matthieu.boudin@etu.sorbonne-universite.fr>
2024-05-06 10:09:54 +0200

    Modif Playboard

---
+++ playboard.cpp
@@ -0,0 +1,39 @@
+#include "cell.hpp"
+
+#include <iostream>
+#include <vector>
+
+Playboard::Playboard(int n, int m) : rows(n), cols(m) {
+    // Initialiser le plateau avec des cellules vides 
+    board = std::vector<std::vector<Cell>>(rows, std::vector<Cell>(cols, Cell()));
+}
+
+void Playboard::printBoard() const {
+    for (const auto& row : board) {
+        for (char cell : row) {
+            std::cout << cell << ' ';
+        }
+        std::cout << std::endl;
+    }
+}
+
+bool Playboard::isValidCell(int row, int col) const {
+    return row >= 0 && row < rows && col >= 0 && col < cols;
+}
+
+void Playboard::setCell(int row, int col, Cell cell) {
+    if (isValidCell(row, col)) {
+        board[row][col] = cell;
+    } else {
+        std::cout << "Coordonnées invalides !" << std::endl;
+    }
+}
+
+char Playboard::getCell(int row, int col) const {
+    if (isValidCell(row, col)) {
+        return board[row][col];
+    } else {
+        std::cout << "Coordonnées invalides !" << std::endl;
+        return ' ';
+    }
+}
--- playboard.hpp
+++ playboard.hpp
@@ -7,42 +7,13 @@ class Playboard {
 private:
     int rows;
     int cols;
-    std::vector<std::vector<char>> board;
+    std::vector<std::vector<Cell>> board;
 
 public:
-    Playboard(int n, int m) : rows(n), cols(m) {
-        // Initialiser le plateau avec des cellules vides (' ')
-        board = std::vector<std::vector<char>>(rows, std::vector<char>(cols, ' '));
-    }
+    Playboard(int n, int m) : rows(n), cols(m);
 
-    void printBoard() const {
-        for (const auto& row : board) {
-            for (char cell : row) {
-                std::cout << cell << ' ';
-            }
-            std::cout << std::endl;
-        }
-    }
-
-    void setCell(int row, int col, char value) {
-        if (isValidCell(row, col)) {
-            board[row][col] = value;
-        } else {
-            std::cout << "Coordonnées invalides !" << std::endl;
-        }
-    }
-
-    char getCell(int row, int col) const {
-        if (isValidCell(row, col)) {
-            return board[row][col];
-        } else {
-            std::cout << "Coordonnées invalides !" << std::endl;
-            return ' ';
-        }
-    }
-
-private:
-    bool isValidCell(int row, int col) const {
-        return row >= 0 && row < rows && col >= 0 && col < cols;
-    }
+    void printBoard() const;
+    bool isValidCell(int row, int col) const;
+    void setCell(int row, int col, char value);
+    char getCell(int row, int col) const;
 };
