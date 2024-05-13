dd2b995da052dbb47a74513db76fe9e7b70c74ce
Victor-BB <162695672+Victor-BB@users.noreply.github.com>
2024-05-13 09:17:09 +0200

    Update cavalier.cpp

--- cavalier.cpp
+++ cavalier.cpp
@@ -13,99 +13,131 @@ Cavalier::Cavalier(int x, int y) {
 
 int Cavalier::deplacement(int choix, Playboard& pboard) {
     switch (choix) {
-        case 1:
-            // 2 haut 1 gauche : Actions à effectuer si choix est égal à 1
+        case 0:
+            // 2 haut 1 gauche : Actions à effectuer si choix est égal à 0
             int newX = posx - 1 ;
             int newY = posy - 2 ;
-            if (isValidMove(newX, newY, pboard)) {
+            if (!pboard.isValidCell(newX,newY) && !pboard.getCell(newX,newY).getStatus()) {
+                printf("deplacement pas fait");
+                return 0;   
+            }
+            else{
                 posx = newX ;
                 posy = newY ;
+                std::cout << "2 haut 1 gauche" << std::endl;
+                return 1 ;
             }
-            std::cout << "2 haut 1 gauche" << std::endl;
-            return 1 ;
 
-        case 2:
-            // 2 haut 1 droite : Actions à effectuer si choix est égal à 2
+
+        case 1:
+            // 2 haut 1 droite : Actions à effectuer si choix est égal à 1
             int newX = posx + 1 ;
             int newY = posy - 2 ;
-            if (isValidMove(newX, newY, pboard)) {
+            if (!pboard.isValidCell(newX,newY) && !pboard.getCell(newX,newY).getStatus()) {
+                printf("deplacement pas fait");
+                return 0;   
+            }
+            else{
                 posx = newX ;
                 posy = newY ;
+                std::cout << "2 haut 1 droite" << std::endl;
+                return 1 ;
             }
-            std::cout << "2 haut 1 droite" << std::endl;
-            return 1 ;
+
+        case 2:
+            // 2 droite 1 haut : Actions à effectuer si choix est égal à 2
+            int newX = posx + 2 ;
+            int newY = posy - 1 ;
+            if (!pboard.isValidCell(newX,newY) && !pboard.getCell(newX,newY).getStatus()) {
+                printf("deplacement pas fait");
+                return 0;   
+            }
+            else{
+                posx = newX ;
+                posy = newY ;
+                std::cout << "2 droite 1 haute" << std::endl;
+                return 1 ;
+            }
 
 
         case 3:
-            // 2 droite 1 haut : Actions à effectuer si choix est égal à 3
-            int newX = posx + 2 ;
-            int newY = posy - 1 ;
-            if (isValidMove(newX, newY, pboard)) {
-                posx = newX ;
-                posy = newY ;
-            }
-            std::cout << "2 droite 1 haut" << std::endl;
-            return 1 ;
-
-
-        case 4:
             // 2 droite 1 bas : Actions à effectuer si choix est égal à 3
             int newX = posx + 2 ;
             int newY = posy + 1 ;
-            if (isValidMove(newX, newY, pboard)) {
+            if (!pboard.isValidCell(newX,newY) && !pboard.getCell(newX,newY).getStatus()) {
+                printf("deplacement pas fait");
+                return 0;   
+            }
+            else{
                 posx = newX ;
                 posy = newY ;
+                std::cout << "2 droite 1 bas" << std::endl;
+                return 1 ;
             }
-            std::cout << "2 droite 1 bas" << std::endl;
-            return 1 ;
+
+
+        case 4:
+            // Actions à effectuer si choix est égal à 4
+            int newX = posx + 1 ;
+            int newY = posy + 2 ;
+             if (!pboard.isValidCell(newX,newY) && !pboard.getCell(newX,newY).getStatus()) {
+                printf("deplacement pas fait");
+                return 0;   
+            }
+            else{
+                posx = newX ;
+                posy = newY ;
+                std::cout << "2 bas 1 droite" << std::endl;
+                return 1 ;
+            }
 
 
         case 5:
-            // Actions à effectuer si choix est égal à 3
-            int newX = posx + 1 ;
+            // Actions à effectuer si choix est égal à 5
+            int newX = posx - 1 ;
             int newY = posy + 2 ;
-            if (isValidMove(newX, newY, pboard)) {
+            if (!pboard.isValidCell(newX,newY) && !pboard.getCell(newX,newY).getStatus()) {
+                printf("deplacement pas fait");
+                return 0;   
+            }
+            else{
                 posx = newX ;
                 posy = newY ;
+                std::cout << "2 bas 1 gauche" << std::endl;
+                return 1 ;
             }
-            std::cout << "2 bas 1 droite" << std::endl;
-            return 1 ;
 
 
         case 6:
-            // Actions à effectuer si choix est égal à 3
-            int newX = posx - 1 ;
-            int newY = posy + 2 ;
-            if (isValidMove(newX, newY, pboard)) {
-                posx = newX ;
-                posy = newY ;
-            }
-            std::cout << "2 bas 1 gauche" << std::endl;
-            return 1 ;
-
-
-        case 7:
-            // Actions à effectuer si choix est égal à 3
+            // Actions à effectuer si choix est égal à 6
             int newX = posx - 2 ;
             int newY = posy + 1 ;
-            if (isValidMove(newX, newY, pboard)) {
+            if (!pboard.isValidCell(newX,newY) && !pboard.getCell(newX,newY).getStatus()) {
+                printf("deplacement pas fait");
+                return 0;   
+            }
+            else{
                 posx = newX ;
                 posy = newY ;
+                std::cout << "2 gauche 1 bas" << std::endl;
+                return 1 ;
             }
-            std::cout << "2 gauche 1 bas " << std::endl;
-            return 1 ;
 
         
-        case 8:
-            // Actions à effectuer si choix est égal à 3
+        case 7:
+            // Actions à effectuer si choix est égal à 7
             int newX = posx - 2 ;
             int newY = posy - 1 ;
-            if (isValidMove(newX, newY, pboard)) {
+               if (!pboard.isValidCell(newX,newY) && !pboard.getCell(newX,newY).getStatus()) {
+                printf("deplacement pas fait");
+                return 0;   
+            }
+            else{
                 posx = newX ;
                 posy = newY ;
+                std::cout << "2 gauche 1 haute" << std::endl;
+                return 1 ;
             }
-            std::cout << "2 gauche 1 haut " << std::endl;
-            return 1 ;
 
 
         default:
