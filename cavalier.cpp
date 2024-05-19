15c714d65057706bba481be0ccf5bd9d4207b67c
Victor-BB <162695672+Victor-BB@users.noreply.github.com>
2024-05-19 22:49:56 +0200

    cavalier

--- cavalier.cpp
+++ cavalier.cpp
@@ -6,9 +6,9 @@ int Cavalier::deplacement(int choix, Playboard& pboard) {
     int newX, newY;
     switch (choix) {
         case 0:
-            // 2 gzuche 1 haut : Actions à effectuer si choix est égal à 0
-            newX = posx - 1 ;
-            newY = posy - 2 ;
+            // 2 gauche 1 haut : Actions à effectuer si choix est égal à 0
+            newX = posx - 2 ;
+            newY = posy - 1 ;
             if (!pboard.isValidCell(newX,newY) || !pboard.getCell(newX,newY).getStatus()) {
                 printf("deplacement pas fait");
                 return 0;   
@@ -21,8 +21,8 @@ int Cavalier::deplacement(int choix, Playboard& pboard) {
             }
         case 1:
             // 2 gauche 1 bas : Actions à effectuer si choix est égal à 1
-            newX = posx + 1 ;
-            newY = posy - 2 ;
+            newX = posx - 2 ;
+            newY = posy + 1 ;
             if (!pboard.isValidCell(newX,newY) || !pboard.getCell(newX,newY).getStatus()) {
                 printf("deplacement pas fait");
                 return 0;   
@@ -36,8 +36,8 @@ int Cavalier::deplacement(int choix, Playboard& pboard) {
 
         case 2:
             // 2 bas 1 gauche : Actions à effectuer si choix est égal à 2
-            newX = posx + 2 ;
-            newY = posy - 1 ;
+            newX = posx - 1 ;
+            newY = posy + 2 ;
             if (!pboard.isValidCell(newX,newY) || !pboard.getCell(newX,newY).getStatus()) {
                 printf("deplacement pas fait");
                 return 0;   
@@ -52,8 +52,8 @@ int Cavalier::deplacement(int choix, Playboard& pboard) {
 
         case 3:
             // 2 bas 1 droite : Actions à effectuer si choix est égal à 3
-            newX = posx + 2 ;
-            newY = posy + 1 ;
+            newX = posx + 1 ;
+            newY = posy + 2 ;
             if (!pboard.isValidCell(newX,newY) || !pboard.getCell(newX,newY).getStatus()) {
                 printf("deplacement pas fait");
                 return 0;   
@@ -61,15 +61,15 @@ int Cavalier::deplacement(int choix, Playboard& pboard) {
             else{
                 posx = newX ;
                 posy = newY ;
-                std::cout << "2 droite 1 bas" << std::endl;
+                std::cout << "2 bas 1 droite" << std::endl;
                 return 1 ;
             }
 
 
         case 4:
             // Actions à effectuer si choix est égal à 4
-            newX = posx + 1 ;
-            newY = posy + 2 ;
+            newX = posx + 2 ;
+            newY = posy + 1 ;
              if (!pboard.isValidCell(newX,newY) || !pboard.getCell(newX,newY).getStatus()) {
                 printf("deplacement pas fait");
                 return 0;   
@@ -84,8 +84,8 @@ int Cavalier::deplacement(int choix, Playboard& pboard) {
 
         case 5:
             // Actions à effectuer si choix est égal à 5
-            newX = posx - 1 ;
-            newY = posy + 2 ;
+            newX = posx + 2 ;
+            newY = posy - 1 ;
             if (!pboard.isValidCell(newX,newY) || !pboard.getCell(newX,newY).getStatus()) {
                 printf("deplacement pas fait");
                 return 0;   
@@ -100,8 +100,8 @@ int Cavalier::deplacement(int choix, Playboard& pboard) {
 
         case 6:
             // Actions à effectuer si choix est égal à 6
-            newX = posx - 2 ;
-            newY = posy + 1 ;
+            newX = posx + 1 ;
+            newY = posy - 2 ;
             if (!pboard.isValidCell(newX,newY) || !pboard.getCell(newX,newY).getStatus()) {
                 printf("deplacement pas fait");
                 return 0;   
@@ -116,8 +116,8 @@ int Cavalier::deplacement(int choix, Playboard& pboard) {
         
         case 7:
             // Actions à effectuer si choix est égal à 7
-            newX = posx - 2 ;
-            newY = posy - 1 ;
+            newX = posx - 1 ;
+            newY = posy - 2 ;
                if (!pboard.isValidCell(newX,newY) || !pboard.getCell(newX,newY).getStatus()) {
                 printf("deplacement pas fait");
                 return 0;   
@@ -185,4 +185,36 @@ void Cavalier::convertXYtoChoiceRange(int i, int j,int* choice,int* range){
         return;
     }
     *choice = -1;
+}
+
+
+int Cavalier::pouvoir(Piece& piece_adverse, Playboard& pboard) {
+    // Lancer d'un dé pour déterminer le pouvoir utilisé
+    int dice_roll = std::rand() % 6 + 1;
+    int choice;
+    int range;
+    //FONCTION MATTHIEU RÉCUP I et J
+    // Pouvoir 1 : Avancer d'une case et faire reculer le joueur adverse d'une case 
+    if (dice_roll == 1 || dice_roll == 3 || dice_roll == 5) {
+        this->convertXYtoChoiceRange (i,j,&choice,&range);
+        if (this->deplacement(choice, pboard)) {
+            piece_adverse.Reculer(1, pboard); // Faire reculer le joueur adverse d'une case
+            return 1;  // Succès du pouvoir 1
+        }
+    }
+    // Pouvoir 2 : Avancer d'une case et faire reculer le joueur adverse de 3 cases (tomber sur un nombre pair)
+    else if (dice_roll == 2 || dice_roll == 4) {
+        if (this->deplacement(choice, pboard)) {
+            piece_adverse.Reculer(3, pboard); // Faire reculer le joueur adverse de 3 cases
+            return 2; // Succès du pouvoir 2
+        }
+    }
+    // Pouvoir 3 : Avancer d'une case et faire reculer le joueur adverse de 5 cases (tomber sur 6)
+    else if (dice_roll == 6) {
+        if (this->deplacement(choice, pboard)) {
+            piece_adverse.Reculer(5, pboard); // Reculer de 5 cases
+            return 3; // Succès du pouvoir 3
+        }
+    }
+    return 0; // Aucun pouvoir n'a été activé
 }
